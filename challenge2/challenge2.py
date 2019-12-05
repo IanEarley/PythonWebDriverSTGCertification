e7c5d6209fe11eaaab135dd1117fe952c3a35e7b
Ian Earley <ianearley@Ians-MacBook-Pro.local>
2019-12-05 16:15:55 -0700

    added wait statement

--- challenge2/challenge2.py
+++ challenge2/challenge2.py
@@ -1,7 +1,9 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class Challenge2 (unittest.TestCase) :
     def setUp(self) :
@@ -16,6 +18,7 @@ class Challenge2 (unittest.TestCase) :
         elem = driver.find_element_by_id("input-search")
         elem.send_keys("exotics")
         elem.send_keys(Keys.RETURN)
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "serverSideDataTable")))
         assert driver.page_source.find("PORSCHE"), "Porsche not present in list"
 
 
